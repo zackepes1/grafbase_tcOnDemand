702e7e6ea13efe6651121dc7c331a9a909540548
Epes <zachary.epes@siemens.com>
2023-12-19 11:35:38 -0500

    First Commit

---
+++ grafbase.config.ts
@@ -0,0 +1,33 @@
+import { g, graph, config } from '@grafbase/sdk'
+
+// Welcome to Grafbase!
+//
+// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+
+
+
+const User = g.model('User', {
+  name: g.string().length({min: 2, max: 20}),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
+})
+
+const Project = g.model('Project', {
+  title: g.string().length({min: 3}),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+})
+
+export default config({
+  graph: g
+
+ 
+})
